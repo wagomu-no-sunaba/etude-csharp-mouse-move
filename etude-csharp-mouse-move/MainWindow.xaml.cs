dba18bdf8cec670a0c6d589dd08ca87690b79e04
staticWagomU <to6wa17@gmail.com>
2024-03-15 08:46:03 +0900

    マウスをスムーズに動かしてみた

--- etude-csharp-mouse-move/MainWindow.xaml.cs
+++ etude-csharp-mouse-move/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace etude_csharp_mouse_move
 {
@@ -16,9 +8,46 @@ namespace etude_csharp_mouse_move
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+#pragma warning disable SYSLIB1054 // コンパイル時に P/Invoke マーシャリング コードを生成するには、'DllImportAttribute' の代わりに 'LibraryImportAttribute' を使用します
+        static extern bool SetCursorPos(int x, int y);
+#pragma warning restore SYSLIB1054 // コンパイル時に P/Invoke マーシャリング コードを生成するには、'DllImportAttribute' の代わりに 'LibraryImportAttribute' を使用します
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var startX = 100;
+            var startY = 100;
+            var endX = 500;
+            var endY = 500;
+            var duration = 1000; // ミリ秒
+
+            var mouseThread = new Thread(() =>
+            {
+                DateTime startTime = DateTime.Now;
+
+                while (true)
+                {
+                    var elapsed = (DateTime.Now - startTime).TotalMilliseconds;
+                    var t = Math.Min(elapsed / duration, 1.0);
+
+                    var currentX = (int)Math.Round(startX + (endX - startX) * t);
+                    var currentY = (int)Math.Round(startY + (endY - startY) * t);
+
+                    SetCursorPos(currentX, currentY);
+
+                    if (t >= 1.0) break;
+
+                    Thread.Sleep(10); // 10ミリ秒ごとに更新
+                }
+            });
+
+            mouseThread.Start();
         }
     }
 }
