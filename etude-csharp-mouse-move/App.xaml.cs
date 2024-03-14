28b00e0343d7b4940207354ae2591ce72869616c
staticWagomU <to6wa17@gmail.com>
2024-03-15 08:14:37 +0900

    プロジェクト ファイルを追加します。

---
+++ etude-csharp-mouse-move/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace etude_csharp_mouse_move
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
